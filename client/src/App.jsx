419a48baa6e0be9f162fb89cbe52ff6131249186
Jacob Lower <jake@claritycoders.com>
2023-02-07 15:28:22 -0600

    Updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,12 @@ import { themeSettings } from "./theme";
 import Dashboard from "./scenes/dashboard";
 import Layout from "./scenes/layout";
 import Products from "./scenes/products";
+import Customers from "./scenes/customers";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  console.log("APP MAIN");
   return (
     <div className="app">
       <BrowserRouter>
@@ -22,6 +24,7 @@ function App() {
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
             </Route>
           </Routes>
         </ThemeProvider>
